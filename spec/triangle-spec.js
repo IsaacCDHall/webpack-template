9aa321d5f044f2c06198d1a58ff69b6a2171a264
Drew Yoxsimer and Isaac Hall <isaac@>
2019-03-12 15:27:45 -0700

    add beforeEach function

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -1,3 +1,4 @@
+import { Triangle } from './../src/triangle.js';
 describe('Triangle', function() {
 
   it('should test whether a Triangle has three sides', function() {
@@ -5,6 +6,22 @@ describe('Triangle', function() {
     expect(triangle.side1).toEqual(3);
     expect(triangle.side2).toEqual(4);
     expect(triangle.side3).not.toEqual(6);
+    console.log(triangle);
+  });
+  it('should correctly determine whether three lengths can be made into a triangle', function() {
+    var notTriangle = new Triangle(3,9,22);
+    expect(notTriangle.checkType()).toEqual("not a triangle");
+  });
 
+
+  var reusableTriangle;
+
+  beforeEach(function() {
+    reusableTriangle = new Triangle(5, 5, 5);
+    console.log("before each");
+  });
+
+  it('should show how beforeEach() works', function() {
+    console.log(reusableTriangle);
   });
 });
