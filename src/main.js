fcc02954da872619b19654a46cd739ffa68b202f
Student <example@example.com>
2019-03-12 10:41:38 -0700

    add file loader for images

--- src/main.js
+++ src/main.js
@@ -1,14 +1,70 @@
-import { pingPong } from './ping-pong';
+// import { pingPong } from './ping-pong';
 import './styles.css';
 
+// $(document).ready(function() {
+//   $('#ping-pong-form').submit(function(event) {
+//     event.preventDefault();
+//     var goal = $('#goal').val();
+//     var output = pingPong(goal);
+//     output.forEach(function(element) {
+//       $('#solution').append("<li>" + element + "</li>");
+//     });
+//   });
+// });
+function nTriangle(side1, side2, side3) {
+  if((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function equilateral(side1, side2, side3) {
+  if(side1 === side2 && side2 === side3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isosceles(side1, side2, side3) {
+  if(side1 === side2 || side1 === side3 || side2 === side3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+//
+// function scalene(side1, side2, side3) {
+//   if(side1 !== side2 && side1 !== side3 && side2 !== side3) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+//jQuery shit
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+  $("form").submit(function(event){
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    let side1 = parseFloat($('#side1Length').val());
+    let side2 = parseFloat($('#side2Length').val());
+    let side3 = parseFloat($('#side3Length').val());
+    let result = "";
+
+    if (nTriangle(side1, side2, side3) === true) {
+      result = "Not a Triangle";
+    } else if(equilateral(side1, side2, side3) === true) {
+      result = "Equilateral Triangle";
+    } else if(isosceles(side1, side2, side3) === true) {
+      result = "Isoceles Triangle";
+    } else {
+      result = "Scalene Triangle";
+    }
+
+
+    $("#output-nTriangle").text(result);
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,26 @@ module: {
         ]
       },
       {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
